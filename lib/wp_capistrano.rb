28318d8a0d6e851493c4c443f762ddb7c022357d
Tom Adams <tom@holizz.com>
2010-08-27 13:24:19 +0100

    Move some files around.

---
+++ lib/wp_capistrano.rb
@@ -0,0 +1,92 @@
+require 'erb'
+require 'digest'
+require 'digest/sha1'
+
+require 'wp_config'
+require 'wp_capistrano/deploy'
+require 'wp_capistrano/setup'
+
+Capistrano::Configuration.instance.load do
+  default_run_options[:pty] = true
+
+  def set_target target
+    tt = WPConfig.instance.h['deploy'][target]
+    if tt
+      t = OpenStruct.new(tt)
+      set :domain, t.ssh_domain
+      set :user, t.ssh_user
+      set :deploy_to, t.path
+      set :wordpress_domain, t.vhost
+      set :wordpress_domain, 'localhost' if wordpress_domain.nil?
+      set :wordpress_db_name, t.database.name
+      set :wordpress_db_user, t.database.user
+      set :wordpress_db_user, 'root' if wordpress_db_user.nil?
+      set :wordpress_db_password, t.database.password
+      set :wordpress_db_host, t.database.host
+      set :wordpress_db_host, 'localhost' if wordpress_db_host.nil?
+      set :use_sudo, t.use_sudo
+      set :deploy_profile, t
+      deploy_profile.modules = [] unless deploy_profile.modules
+
+      @roles = {}
+      role :app, domain
+      role :web, domain
+      role :db,  domain, :primary => true
+    end
+  end
+
+  WPConfig.instance.h['deploy'].each_pair do |k,v|
+    set_target k if v['default']
+  end
+
+  task :testing do
+    set_target 'testing'
+  end
+  task :staging do
+    set_target 'staging'
+  end
+  task :production do
+    set_target 'production'
+  end
+
+  # Load from config
+  set :wordpress_version, WPConfig.wordpress.version
+  set :wordpress_git_url, WPConfig.wordpress.repository
+  set :repository, WPConfig.application.repository
+
+  # Everything else
+  set :scm, "git"
+  set :deploy_via, :remote_cache
+  set :branch, "master"
+  set :git_shallow_clone, 1
+  set :git_enable_submodules, 1
+  set :wordpress_db_host, "localhost"
+  set :wordpress_auth_key, Digest::SHA1.hexdigest(rand.to_s)
+  set :wordpress_secure_auth_key, Digest::SHA1.hexdigest(rand.to_s)
+  set :wordpress_logged_in_key, Digest::SHA1.hexdigest(rand.to_s)
+  set :wordpress_nonce_key, Digest::SHA1.hexdigest(rand.to_s)
+
+  #allow deploys w/o having git installed locally
+  set(:real_revision) do
+    output = ""
+    invoke_command("git ls-remote #{repository} #{branch} | cut -f 1", :once => true) do |ch, stream, data|
+      case stream
+      when :out
+        if data =~ /\(yes\/no\)\?/ # first time connecting via ssh, add to known_hosts?
+          ch.send_data "yes\n"
+        elsif data =~ /Warning/
+        elsif data =~ /yes/
+          #
+        else
+          output << data
+        end
+      when :err then warn "[err :: #{ch[:server]}] #{data}"
+      end
+    end
+    output.gsub(/\\/, '').chomp
+  end
+
+  #no need for log and pids directory
+  set :shared_children, %w(system)
+
+end
--- lib/wp_capistrano/deploy.rb
+++ lib/wp_capistrano/deploy.rb
@@ -1,92 +1,175 @@
-require 'wp_config'
-require 'erb'
-require 'digest'
-require 'digest/sha1'
-
-require 'wp_capistrano/namespace/deploy'
-require 'wp_capistrano/namespace/setup'
+require 'wp_capistrano' # Compatability
 
 Capistrano::Configuration.instance.load do
-  default_run_options[:pty] = true
+  namespace :deploy do
 
-  def set_target target
-    tt = WPConfig.instance.h['deploy'][target]
-    if tt
-      t = OpenStruct.new(tt)
-      set :domain, t.ssh_domain
-      set :user, t.ssh_user
-      set :deploy_to, t.path
-      set :wordpress_domain, t.vhost
-      set :wordpress_domain, 'localhost' if wordpress_domain.nil?
-      set :wordpress_db_name, t.database.name
-      set :wordpress_db_user, t.database.user
-      set :wordpress_db_user, 'root' if wordpress_db_user.nil?
-      set :wordpress_db_password, t.database.password
-      set :wordpress_db_host, t.database.host
-      set :wordpress_db_host, 'localhost' if wordpress_db_host.nil?
-      set :use_sudo, t.use_sudo
-      set :deploy_profile, t
-      deploy_profile.modules = [] unless deploy_profile.modules
-
-      @roles = {}
-      role :app, domain
-      role :web, domain
-      role :db,  domain, :primary => true
+    desc "Override deploy restart to not do anything"
+    task :restart do
     end
-  end
 
-  WPConfig.instance.h['deploy'].each_pair do |k,v|
-    set_target k if v['default']
-  end
+    task :finalize_update, :except => { :no_release => true } do
+      run "chmod -R g+w #{latest_release}"
 
-  task :testing do
-    set_target 'testing'
-  end
-  task :staging do
-    set_target 'staging'
-  end
-  task :production do
-    set_target 'production'
-  end
+      # I've got submodules in my submodules
+      #run "cd #{latest_release} && git submodule foreach --recursive git submodule update --init"
+      # Git 1.5-compatability:
+      run "cd #{latest_release} && DIR=`pwd` && for D in `grep '^\\[submodule' .git/config | cut -d\\\" -f2`; do cd $DIR/$D && git submodule init && git submodule update; done"
 
-  # Load from config
-  set :wordpress_version, WPConfig.wordpress.version
-  set :wordpress_git_url, WPConfig.wordpress.repository
-  set :repository, WPConfig.application.repository
+      deploy.sass
 
-  # Everything else
-  set :scm, "git"
-  set :deploy_via, :remote_cache
-  set :branch, "master"
-  set :git_shallow_clone, 1
-  set :git_enable_submodules, 1
-  set :wordpress_db_host, "localhost"
-  set :wordpress_auth_key, Digest::SHA1.hexdigest(rand.to_s)
-  set :wordpress_secure_auth_key, Digest::SHA1.hexdigest(rand.to_s)
-  set :wordpress_logged_in_key, Digest::SHA1.hexdigest(rand.to_s)
-  set :wordpress_nonce_key, Digest::SHA1.hexdigest(rand.to_s)
+      run <<-CMD
+        mkdir -p #{latest_release}/finalized &&
+        cp -rv   #{shared_path}/wordpress/*     #{latest_release}/finalized/ &&
+        rm -rf   #{latest_release}/finalized/wp-content &&
+        mkdir    #{latest_release}/finalized/wp-content &&
+        rm -rf #{latest_release}/**/.git &&
+        cp -rv #{latest_release}/themes  #{latest_release}/finalized/wp-content/ &&
+        ln -s #{shared_path}/uploads   #{latest_release}/finalized/wp-content/ &&
+        mkdir -p #{latest_release}/finalized/wp-content/cache/ ;
+        chmod -R 777 #{latest_release}/finalized/wp-content/cache/ ;
+        true
+      CMD
 
-  #allow deploys w/o having git installed locally
-  set(:real_revision) do
-    output = ""
-    invoke_command("git ls-remote #{repository} #{branch} | cut -f 1", :once => true) do |ch, stream, data|
-      case stream
-      when :out
-        if data =~ /\(yes\/no\)\?/ # first time connecting via ssh, add to known_hosts?
-          ch.send_data "yes\n"
-        elsif data =~ /Warning/
-        elsif data =~ /yes/
-          #
-        else
-          output << data
+      if deploy_profile.modules.include? 'shared-plugins'
+        run("ln -s #{shared_path}/plugins #{latest_release}/finalized/wp-content/")
+      else
+        run("cp -rv #{latest_release}/plugins #{latest_release}/finalized/wp-content/")
+      end
+
+      deploy.wp_config
+
+      ## WP Super Cache
+
+      if File.exist? 'plugins/wp-super-cache/advanced-cache.php'
+        top.upload("plugins/wp-super-cache/advanced-cache.php", "#{latest_release}/finalized/wp-content/" , :via => :scp)
+        sedable_path = "#{latest_release}/finalized/wp-content/plugins/wp-super-cache/".gsub(/\//,'\/')
+        run("sed -i 's/CACHEHOME/#{sedable_path}/g' #{latest_release}/finalized/wp-content/advanced-cache.php")
+      else
+        raise IOError, 'Are you sure you have the WP Super Cache plugin?'
+      end
+
+      if File.exist? 'wp-cache-config.php'
+        top.upload("wp-cache-config.php", "#{latest_release}/finalized/wp-content/" , :via => :scp)
+      elsif File.exist? 'plugins/wp-super-cache/wp-cache-config-sample.php'
+        top.upload("plugins/wp-super-cache/wp-cache-config-sample.php", "#{latest_release}/finalized/wp-content/wp-cache-config.php" , :via => :scp)
+      else
+        raise IOError, 'Are you sure you have the WP Super Cache plugin?'
+      end
+
+      run("mkdir -p #{latest_release}/finalized/wp-content/cache/blogs &&
+           mkdir -p #{latest_release}/finalized/wp-content/cache/meta")
+
+      # Keep writables to a bare minimum
+      if deploy_profile.modules.include? 'wp-super-cache'
+        run("chmod -R 777 #{latest_release}/finalized/wp-content/cache &&
+             chmod -R 777 #{latest_release}/finalized/wp-content/wp-cache-config.php")
+      end
+
+      ## Custom htaccess
+
+      if File.exist? 'htaccess'
+        top.upload("htaccess", "#{latest_release}/finalized/.htaccess" , :via => :scp)
+      end
+
+      ## Allow plugin installation
+
+      if deploy_profile.modules.include? 'plugin-install'
+
+        run("mkdir -p #{latest_release}/finalized/wp-content/upgrade &&
+            chmod -R 777 #{latest_release}/finalized/wp-content/upgrade &&
+            chmod -R 777 #{latest_release}/finalized/wp-content/plugins ; true")
+
+      end
+
+      ## Cforms
+
+      cforms_dir = "#{latest_release}/finalized/wp-content/plugins/cforms"
+      run(%Q%[ -e #{cforms_dir} ] && echo '<?php $abspath="#{latest_release}/finalized/" ?>' > #{cforms_dir}/abspath.php ; true%)
+
+    end
+
+    desc "Copy shared wp-config into place, adding per-deploy constants"
+    task :wp_config do
+      ## Generate a couple of PHP fragments
+
+      # nb:
+      # - in a string=>string pair, the value represents a PHP literal (i.e. it includes the quotes)
+      #   the key, however, is a string
+      # - the convention is to put things into preconfig unless there's a reason not to
+      preconfig = {}
+      postconfig = {}
+      # For certain cases we may allow the user write access (i.e. module plugin-install)
+      # it should always be direct filesystem access (and WordPress' autodetection is pants)
+      preconfig['FS_METHOD'] = "'direct'"
+
+      # Modules
+      if deploy_profile.modules.include? 'wp-super-cache'
+        preconfig['WP_CACHE'] = "'true'"
+      end
+      if deploy_profile.modules.include? 'shared-plugins'
+        preconfig['WP_PLUGIN_DIR'] = "'"+"#{shared_path}/plugins".gsub(/'/,"\\'")+"'"
+      end
+
+      def phpize(h)
+        s = []
+        h.each_pair do |k,v|
+          s << "define('#{k}', #{v});"
         end
-      when :err then warn "[err :: #{ch[:server]}] #{data}"
+        s.join("\n")
+      end
+
+      prestring = phpize(preconfig)
+      poststring = phpize(postconfig)
+
+      ## Upload fragments
+
+      run "mkdir -p #{latest_release}/build"
+      put prestring, "#{latest_release}/build/pre-config"
+      put poststring, "#{latest_release}/build/post-config"
+
+      ## sed them into wp-config (using perl)
+      r = "cp -rv #{shared_path}/wp-config.php #{latest_release}/wp-config.php &&"
+
+      %w[pre post].each do |pp|
+        f = "#{latest_release}/build/#{pp}-config"
+        r += %Q`perl -i -pe 'BEGIN{undef $/;open(F,"<#{f}");@f=<F>;$f=join("",@f);}s/## #{pp.upcase}CONFIG BEGIN.*## #{pp.upcase}CONFIG END./$f/ms' #{latest_release}/wp-config.php &&`
+      end
+
+      r += "cp -rv #{latest_release}/wp-config.php #{latest_release}/finalized/wp-config.php"
+
+      run r
+
+    end
+
+    desc "Compile SASS locally and upload it"
+    task :sass do
+      Dir.glob("themes/*/*/sass_output.php").map {|d| d.match(%r&/([^/]+)/([^/]+)/sass_output.php$&)[1,2]}[0..0].each do |theme_dir,sass_dir|
+        f = IO.popen("cd themes/#{theme_dir}/#{sass_dir} && php sass_output.php 2>/dev/null")
+        sass_output = f.readlines.join
+        remote_sass = "#{latest_release}/themes/#{theme_dir}/#{sass_dir}/sass_output.css"
+        put(sass_output, remote_sass, :via => :scp)
+        run("sed -i 's/\.php/\.css/' #{latest_release}/themes/#{theme_dir}/style.css &&
+            chmod a+r #{remote_sass}")
       end
     end
-    output.gsub(/\\/, '').chomp
+
+    task :symlink, :except => { :no_release => true } do
+      on_rollback do
+        if previous_release
+          run "rm -f #{current_path}; ln -s #{previous_release}/finalized #{current_path}; true"
+        else
+          logger.important "no previous release to rollback to, rollback of symlink skipped"
+        end
+      end
+
+      run "rm -f #{current_path} && ln -s #{latest_release}/finalized #{current_path}"
+    end
+
+    namespace :revision do
+      task :revision do
+        puts 'hhh'
+      end
+    end
+
   end
-
-  #no need for log and pids directory
-  set :shared_children, %w(system)
-
 end
