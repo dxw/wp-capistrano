c96993d69d032d63f6eaab922838504f78068105
Tom Adams <tom@holizz.com>
2010-09-07 11:52:36 +0100

    wp-super-cache module was doing silly things.

--- lib/wp-capistrano/feature/wp-super-cache.rb
+++ lib/wp-capistrano/feature/wp-super-cache.rb
@@ -14,23 +14,23 @@ Capistrano::Configuration.instance.load do
       top.upload("plugins/wp-super-cache/advanced-cache.php", "#{latest_release}/finalized/wp-content/" , :via => :scp)
       sedable_path = "#{latest_release}/finalized/wp-content/plugins/wp-super-cache/".gsub(/\//,'\/')
       run("sed -i 's/CACHEHOME/#{sedable_path}/g' #{latest_release}/finalized/wp-content/advanced-cache.php")
-    else
-      raise IOError, 'Are you sure you have the WP Super Cache plugin?'
+    elsif deploy_profile.modules.include? 'wp-super-cache'
+      raise IOError, 'Are you sure you have the WP Super Cache plugin? Could not find advanced-cache.php'
     end
 
     if File.exist? 'wp-cache-config.php'
       top.upload("wp-cache-config.php", "#{latest_release}/finalized/wp-content/" , :via => :scp)
     elsif File.exist? 'plugins/wp-super-cache/wp-cache-config-sample.php'
       top.upload("plugins/wp-super-cache/wp-cache-config-sample.php", "#{latest_release}/finalized/wp-content/wp-cache-config.php" , :via => :scp)
-    else
-      raise IOError, 'Are you sure you have the WP Super Cache plugin?'
+    elsif deploy_profile.modules.include? 'wp-super-cache'
+      raise IOError, 'Are you sure you have the WP Super Cache plugin? Could not find wp-cache-config.php'
     end
 
-    run("mkdir -p #{latest_release}/finalized/wp-content/cache/blogs &&
-         mkdir -p #{latest_release}/finalized/wp-content/cache/meta")
-
-    # Keep writables to a bare minimum
+    # Keep writable directories to a bare minimum
     if deploy_profile.modules.include? 'wp-super-cache'
+      run("mkdir -p #{latest_release}/finalized/wp-content/cache/blogs &&
+           mkdir -p #{latest_release}/finalized/wp-content/cache/meta")
+
       run("chmod -R 777 #{latest_release}/finalized/wp-content/cache &&
            chmod -R 777 #{latest_release}/finalized/wp-content/wp-cache-config.php")
     end
